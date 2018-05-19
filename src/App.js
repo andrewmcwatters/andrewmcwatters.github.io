c5476c722dcf94bc8d059d7736fcbbb554f40971
Andrew McWatters <mcwattersandrew@gmail.com>
2018-05-19 11:15:38 -0700

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header></Header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </main>
+        <Footer></Footer>
       </div>
     );
   }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer(props) {
+  return <footer>
+    Copyright © 2018 Andrew McWatters
+  </footer>;
+}
+
+export default Footer;
