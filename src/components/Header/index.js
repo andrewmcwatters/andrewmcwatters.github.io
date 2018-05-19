0700df95fd5580ed9740cf6fcc3e8a7ef6b027ee
Andrew McWatters <mcwattersandrew@gmail.com>
2018-05-19 10:27:29 -0700

    Refactor header to Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Header.css';
+
+function Header(props) {
+  return <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">Welcome to React</h1>
+  </header>;
+}
+
+export default Header;
