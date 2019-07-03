a0009a9111374d56c0cf0e84e818d78e6cd895fb
Andrew McWatters <andrewmcwatters@Andrews-iMac.local>
2019-07-02 20:48:20 -0700

    Add footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Home from './routes/Home';
 import NoMatch from './routes/NoMatch';
 import { Route, Switch } from "react-router-dom";
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -15,6 +15,7 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route component={NoMatch} />
         </Switch>
+        <Footer></Footer>
       </React.Fragment>
     );
   }
