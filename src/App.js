97744359671c7ffccb2628b0542b0b90f3c7b049
Andrew McWatters <mcwattersandrew@gmail.com>
2020-04-18 13:54:02 -0700

    Enable route-based code splitting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import './App.css';
 import Header from './components/Header';
-import Home from './routes/Home';
-import NoMatch from './routes/NoMatch';
 import { Route, Switch } from "react-router-dom";
 import Footer from './components/Footer';
+
+const Home = lazy(() => import('./routes/Home'));
+const NoMatch = lazy(() => import('./routes/NoMatch'));
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header></Header>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route component={NoMatch} />
-        </Switch>
+        <Suspense fallback={<main>
+          <div>Loading...</div>
+        </main>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Suspense>
         <Footer></Footer>
       </React.Fragment>
     );
