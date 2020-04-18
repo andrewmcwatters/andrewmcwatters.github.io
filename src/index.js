d0d5764cd1713beb79318f566ad451e1cf5c761d
Andrew McWatters <mcwattersandrew@gmail.com>
2020-04-18 13:53:27 -0700

    Enable React Strict Mode

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 ), document.getElementById('root'));
 registerServiceWorker();
