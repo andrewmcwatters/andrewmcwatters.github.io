bb08fc330076728a3f15cdda39ad346f685667f3
Andrew McWatters <mcwattersandrew@gmail.com>
2020-04-18 13:57:25 -0700

    Updates

---
+++ precache-manifest.f95bec235df2824e39f18b005dfcef03.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637e43a052e4d3253f5889d8970dbba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b083228620d089dc97a",
+    "url": "/static/css/3.79ce2ac9.chunk.css"
+  },
+  {
+    "revision": "ec0c24c30e90ced098e3",
+    "url": "/static/css/main.7e2ec9d6.chunk.css"
+  },
+  {
+    "revision": "ac6d6d6c36cb76efe80a",
+    "url": "/static/js/2.b159eaa2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b159eaa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b083228620d089dc97a",
+    "url": "/static/js/3.4d7a26b8.chunk.js"
+  },
+  {
+    "revision": "fd6c7c294fe90ff53231",
+    "url": "/static/js/4.a36d8968.chunk.js"
+  },
+  {
+    "revision": "ec0c24c30e90ced098e3",
+    "url": "/static/js/main.33346388.chunk.js"
+  },
+  {
+    "revision": "17cf2f597bd9517e190b",
+    "url": "/static/js/runtime-main.459d4e78.js"
+  },
+  {
+    "revision": "f284b352685c934137f71219eeea8155",
+    "url": "/static/media/awwwards.f284b352.svg"
+  },
+  {
+    "revision": "fb67fb875b6092b38a2e8aaafb9fa44d",
+    "url": "/static/media/creative_bloq.fb67fb87.svg"
+  },
+  {
+    "revision": "07bd009d61d3fa387c6a655dd31ee7a9",
+    "url": "/static/media/logo.07bd009d.svg"
+  }
+]);
